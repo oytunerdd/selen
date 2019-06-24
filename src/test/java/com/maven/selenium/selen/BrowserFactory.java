45e7d9d2275ec155b34cf9f59f37d97e1b57caf9
Oytun <oytun@Arslan-MacBook-Air.local>
2019-06-24 16:33:05 +0300

    browser check

--- src/test/java/com/maven/selenium/selen/BrowserFactory.java
+++ src/test/java/com/maven/selenium/selen/BrowserFactory.java
@@ -1,9 +1,11 @@
 package com.maven.selenium.selen;
  
 import java.util.concurrent.TimeUnit;
- 
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
  
 public class BrowserFactory {
@@ -16,12 +18,17 @@ public BrowserFactory(){
 public static void main(String[] args) {
     System.out.println("Hello World!");
     System.setProperty("webdriver.gecko.driver", "/Users/oytun/Downloads/geckodriver");
+    FirefoxOptions firefoxOptions = new FirefoxOptions();
+    firefoxOptions.addArguments("--headless");
+    
     WebDriver driver = new FirefoxDriver();
 	driver.manage().window().maximize();
 	driver.manage().timeouts().implicitlyWait(45, TimeUnit.SECONDS);
 	driver.manage().timeouts().pageLoadTimeout(45, TimeUnit.SECONDS);
 	System.out.println("OPENING DRIVER");
 	driver.get("http://google.com/");
+	System.out.println("BROWSER IS ON");
+	driver.quit();
 
 }
 
