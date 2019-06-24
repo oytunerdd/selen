ea7a475cc0e6fd412c8cb7b44c7a1d4525d19e85
Oytun <oytun@Arslan-MacBook-Air.local>
2019-06-24 12:38:28 +0300

    first commit

---
+++ src/test/java/com/maven/selenium/selen/BrowserFactory.java
@@ -0,0 +1,29 @@
+package com.maven.selenium.selen;
+ 
+import java.util.concurrent.TimeUnit;
+ 
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+ 
+public class BrowserFactory {
+ 
+
+ 
+public BrowserFactory(){
+	  
+}
+public static void main(String[] args) {
+    System.out.println("Hello World!");
+    System.setProperty("webdriver.gecko.driver", "/Users/oytun/Downloads/geckodriver");
+    WebDriver driver = new FirefoxDriver();
+	driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(45, TimeUnit.SECONDS);
+	driver.manage().timeouts().pageLoadTimeout(45, TimeUnit.SECONDS);
+	System.out.println("OPENING DRIVER");
+	driver.get("http://google.com/");
+
+}
+
+
+}
